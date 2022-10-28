fd9f8ad4fc08fa3d15874bec262174593d8db2a4
DevDan <116795827+ChinLivingHome@users.noreply.github.com>
2022-10-28 23:36:41 +0700

    -Include Header, Footer, Content      making header -Include Header Css -Include image Bg

--- src/modules/Home.jsx
+++ src/modules/Home.jsx
@@ -1,9 +1,23 @@
 import React from "react"
+import { Layout } from 'antd';
+import '../assets/scss/Header.css'
+// import { Col, Row } from 'antd';
 
 const Home = () => {
+    const { Header, Footer, Content } = Layout;
     return(
         <>
-            <h1> Chin Living Home </h1>
+        <Layout>
+            <Header class="header-Bgimage">
+                <div class="header-text">
+                    <h1>
+                        Chin Living Home
+                    </h1>
+                </div>
+            </Header>
+            <Content>Content</Content>
+            <Footer>Footer</Footer>
+        </Layout>
         </>
     )
 }
