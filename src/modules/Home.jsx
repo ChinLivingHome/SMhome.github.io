de85ccf49b362ec831cd8595d15b0a6236be1a04
DESKTOP-87E72PS\ADMIN <chinnawat.5768@gmail.com>
2022-10-29 02:13:23 +0700

    -add bg and detail for footer

--- src/modules/Home.jsx
+++ src/modules/Home.jsx
@@ -1,11 +1,14 @@
 import React from "react"
 import 'antd/dist/antd.min.css'
-import { Col, Layout, Row } from 'antd';
+import {
+    PhoneTwoTone, MailTwoTone, EnvironmentTwoTone,
+  } from '@ant-design/icons';
+import { Col, Layout, Row, Typography } from 'antd';
 import '../assets/scss/Home.css'
-
 
 const Home = () => {
     const { Header, Footer, Content } = Layout;
+    const { Title } = Typography;
     return(
         <>
         <Layout>
@@ -20,14 +23,25 @@ const Home = () => {
             </Header>
             <Content>Content</Content>
             <Footer className='footer-bg'>
+                <Row>
+                    <Col span={18}>
+                        <Title> ติดต่อ / Contact </Title>
+                    </Col>
+                    <Col span={6}>
+                        <Title> ที่อยู่ / Location </Title>
+                    </Col>
+                </Row>
                 <Row justify="left">
                     <Col span={18}>
-                        Tel: 099-607-8908
+                        <Title level={2}><PhoneTwoTone /> 099-607-8908 (ENG), 081-865-8818(TH)</Title>
+                    </Col>
+                    <Col span={6}>
+                        <Title level={3}><EnvironmentTwoTone /> 165/7 ม.9 ซอยเฉลิมพระเกียรติ 17 ต.หนองปรือ อ.บางละมุง จ.ชลบุรี 20150 ประเทศไทย </Title>
                     </Col>
                 </Row>
                 <Row>
                     <Col span={18}>
-                        Email: chinnawat.5768@gmail.com
+                        <Title level={2}><MailTwoTone /> chinnawat.5768@gmail.com</Title>
                     </Col>
                 </Row>
             </Footer>
