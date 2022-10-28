56f78c6a45ccf1ae01be5d3bbcddb3e27d148f67
DESKTOP-87E72PS\ADMIN <chinnawat.5768@gmail.com>
2022-10-29 01:15:28 +0700

    - grid set up

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 
-import '../src/assets/scss/App.css';
 import Home from '../src/modules/Home'
 
 const App = () => {
--- src/modules/Home.jsx
+++ src/modules/Home.jsx
@@ -1,15 +1,16 @@
 import React from "react"
+import 'antd/dist/antd.min.css'
 import { Col, Layout, Row } from 'antd';
 import '../assets/scss/Home.css'
-// import { Col, Row } from 'antd';
+
 
 const Home = () => {
     const { Header, Footer, Content } = Layout;
     return(
         <>
         <Layout>
-            <Header>
-                <Row className="header-bg-image">
+            <Header className="header-bg-image">
+                <Row>
                 <div className="header-text">
                     <h1>
                         Chin Living Home
@@ -20,12 +21,12 @@ const Home = () => {
             <Content>Content</Content>
             <Footer className='footer-bg'>
                 <Row justify="left">
-                    <Col span={18} push={6}>
+                    <Col span={18}>
                         Tel: 099-607-8908
                     </Col>
                 </Row>
                 <Row>
-                    <Col span={18} push={6}>
+                    <Col span={18}>
                         Email: chinnawat.5768@gmail.com
                     </Col>
                 </Row>
